2c68b2531526f435dbd0849d21e7b9aa7cb35010
gxr <930771231@qq.com>
2020-05-19 21:05:26 +0800

    品牌管理

--- src/api/product/trademark.js
+++ src/api/product/trademark.js
@@ -1,63 +1,69 @@
 /*
-包含所有品牌管理的接口请求函数
+商品管理的品牌管理相关接口请求函数的模块
 */
-import request from '@/utils/request'
 
-const BASE_PATH = '/admin/product/baseTrademark'
+import request from "@/utils/request"; // 相当于ajax
+
+const api_name = "/admin/product/baseTrademark";
 
 export default {
+  /*
+  根据id获取对应的品牌
+  GET /admin/product/baseTrademark/get/{id}
+  */
+  getById(id) {
+    /* return request({
+      url: `${api_name}/get/${id}`,
+      method: 'GET'
+    }) */
+    return request.get(`${api_name}/get/${id}`);
+  },
 
   /*
-  获取品牌全部列表或者分页列表
-  GET /admin/product/baseTrademark/{page}/{limit}
+  获取所有品牌的列表
   GET /admin/product/baseTrademark/getTrademarkList
+  获取品牌分页列表
+  GET /admin/product/baseTrademark/{page}/{limit}
   */
   getList(page, limit) {
     if (page && limit) {
-      return request({
-        url: `${BASE_PATH}/${page}/${limit}`,
-        method: 'GET',
-      })
+      return request(`/admin/product/baseTrademark/${page}/${limit}`);
     } else {
-      return request.get('/admin/product/baseTrademark/getTrademarkList')
+      return request("/admin/product/baseTrademark/getTrademarkList");
     }
   },
 
   /*
-  添加品牌
-  POST /admin/product/baseTrademark/save
+  根据id删除对应的品牌
+  DELETE /admin/product/baseTrademark/remove/{id}
   */
-  add (trademark) {
-    return request({
-      url: `${BASE_PATH}/save`,
-      method: 'POST',
-      data: trademark
-    })
-    // return request.post(`${BASE_PATH}/update`, trademark)
+  remove(id) {
+    return request.delete(`/admin/product/baseTrademark/remove/${id}`);
+  },
+
+  /*
+  添加一个新的品牌
+  POST /admin/product/baseTrademark/save
+  trademark对象: {tmName: '', logoUrl: ''}   没有id属性
+  */
+  add(trademark) {
+    return request.post(`${api_name}/save`, trademark);
   },
 
   /*
   更新品牌
   PUT /admin/product/baseTrademark/update
+  trademark对象: {id: 2, tmName: '', logoUrl: ''}   需要有id属性
   */
-  update (trademark) {
-    return request({
-      url: `${BASE_PATH}/update`,
-      method: 'PUT',
-      data: trademark
-    })
-    // return request.put(`${BASE_PATH}/update`, trademark)
-  },
+  update(trademark) {
+    return request.put(`${api_name}/update`, trademark);
+  }
 
   /*
-  删除品牌
-  DELETE /admin/product/baseTrademark/remove/{id}
+  获取品牌分页列表
+  GET /admin/product/baseTrademark/{page}/{limit}
   */
-  remove (id) {
-    return request({
-      url: `${BASE_PATH}/remove/${id}`,
-      method: 'delete',
-    })
-    // return request.delete(`${BASE_PATH}/remove/${id}`)
-  },
-}
+  /* getPageList (page, limit) {
+    return request(`/admin/product/baseTrademark/${page}/${limit}`)
+  } */
+};
